fef92be831b3e53dd8f2f68cfc61c0bdd653c92f
kmamike193 <100041867+mixan193@users.noreply.github.com>
2025-06-06 19:10:14 +0300

    Update user.py

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func, Boolean
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 
@@ -11,7 +11,7 @@ class User(Base):
     hashed_password = Column(String(255), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    is_superuser = Column(Integer, nullable=False, default=0)  # 0/1 для совместимости
-    is_active = Column(Integer, nullable=False, default=1)
+    is_superuser = Column(Boolean, nullable=False, default=False)
+    is_active = Column(Boolean, nullable=False, default=True)
 
     wifi_snapshots = relationship("WiFiSnapshot", back_populates="user")
