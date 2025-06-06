764db348f6ae55c7649ed1266689224a65582b4f
kmamike193 <100041867+mixan193@users.noreply.github.com>
2025-06-06 16:34:57 +0300

    Update user.py

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -11,5 +11,7 @@ class User(Base):
     hashed_password = Column(String(255), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_superuser = Column(Integer, nullable=False, default=0)  # 0/1 для совместимости
+    is_active = Column(Integer, nullable=False, default=1)
 
     wifi_snapshots = relationship("WiFiSnapshot", back_populates="user")
